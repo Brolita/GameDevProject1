d1100301d677039643a0939870c894597813c3ce
brolita <byrnec3@rpi.edu>
2014-09-07 16:34:59 -0400

    Changes to bullets and enemies

--- bullet.py
+++ bullet.py
@@ -1,43 +1,55 @@
+import Vector
+import game
+
 class Bullet(pygame.sprite.Sprite):
-	self.game = game
-	def __init__ (self, game, spriteName, generateAtInstantiation = true):
+	def __init__ (self, spriteName, generateAtInstantiation = true):
 		self.spriteName = spriteName
+		self.isGenerated = generateAtInstantiation
 		if(generateAtInstantiation):
 			self.Generate()
 			
 	def Generate (self):
 		pygame.sprite.Sprite.__init__(self)
 		self.image, self.rect = load_png(self.spriteName)
+		self.isGenerated = true	
 		
-class LinearBullet:
+		
+class LinearBullet(Bullet):
 	def __init__(self, init, target, speed, game, spriteName, generateAtInstantiation = true):
 		self.position = init
 		self.target = target
 		self.speed = speed
-		self.bullet = Bullet(self, game, spriteName, generateAtInstantiation)
-	
-	def Update(self):
+		pos = self.position
 		self.position.MoveToward(self.target, self.speed)
+		self.velocity = self.position - pos
+		self.game = game
+		self.game.bullets.append(self)
+		Super(LinearBullet, self).__init__(spriteName, generateAtInstantiation)
 		
-class CircularBullet:
-	def __init__(self, init, target, radialSpeed, radius, game, spriteName, generateAtInstantiation = true):
+	def Update(self):
+		self.position += self.velocity
+		
+class CircularBullet(Bullet):
+	def __init__(self, init, target, radialSpeed, game, spriteName, generateAtInstantiation = true):
 		self.position = init
 		self.target = target
 		self.radialSpeed = radialSpeed
-		self.r = radius
-		self.bullet = Bullet(self, game, spriteName, generateAtInstantiation)
+		self.game = game
+		self.game.bullets.append(self)
+		Super(CircularBullet, self).__init__(priteName, generateAtInstantiation)
 	
 	def Update(self):
 		self.position.RotateAround(self.target, self.angularSpeed/self.game.framerate)
 		
-class SpiralBullet:
-	def __init__(self, init, target, radialSpeed, approachSpeed, radius, game, spriteName, generateAtInstantiation = true):
+class SpiralBullet(Bullet):
+	def __init__(self, init, target, radialSpeed, approachSpeed, game, spriteName, generateAtInstantiation = true):
 		self.position = init
 		self.target = target
 		self.radialSpeed = radialSpeed
 		self.approachSpeed = approachSpeed
-		self.r = radius
-		self.bullet = Bullet(game, spriteName, generateAtInstantiation)
+		self.game = game
+		self.game.bullets.append(self)
+		Super(CircularBullet, self).__init__(spriteName, generateAtInstantiation)
 		
 	def Update(self):
 		self.position.RotateAround(self.target, self,radialSpeed)
