2765a94616d6d69e0d32566b77faa0a61d1a0196
tfarrell1993 <terence.m.farrell@gmail.com>
2014-09-07 15:21:48 -0400

    Basic player controls implemented
    Game boots to player (with placeholder sprite) on empty level (with
    placeholder background).

--- game.py
+++ game.py
@@ -1,12 +1,51 @@
-class Game:
-	def __init__ (self):
-		self.pygame = pygame.init()
-		self.bullets = list()
-		
-	def Update(self):
-		for i in range(len(self.bullets)):
-			self.bullets[i].Update()
-	
-	def Draw(self):
-		for i in range(len(self.bullets)):
-			self.bullets[i].Draw()
+import pygame, sys, random, fileinput
+from player import Player
+
+pygame.init()
+screen = pygame.display.set_mode((600, 800))
+levelOneBackground = pygame.image.load('Art Stuff\level one background.png')
+clock = pygame.time.Clock()
+
+def processPlayerEvents(player):
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			print pygame.key.name(event.key)
+			if event.key == pygame.K_ESCAPE:
+				sys.exit()
+			
+			if event.key == pygame.K_UP:
+				player.moving[0] = True
+				
+			if event.key == pygame.K_DOWN:
+				player.moving[1] = True
+				
+			if event.key == pygame.K_LEFT:
+				player.moving[2] = True
+				
+			if event.key == pygame.K_RIGHT:
+				player.moving[3] = True
+				
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_UP:
+				player.moving[0] = False
+				
+			if event.key == pygame.K_DOWN:
+				player.moving[1] = False
+				
+			if event.key == pygame.K_LEFT:
+				player.moving[2] = False
+				
+			if event.key == pygame.K_RIGHT:
+				player.moving[3] = False
+
+gameRunning = True
+player = Player()
+
+while gameRunning:
+	clock.tick(60)
+	processPlayerEvents(player)
+	screen.blit(levelOneBackground, levelOneBackground.get_rect(), [0, 0, 600, 800])
+	player.update()
+	player.draw(screen)
+	pygame.display.flip()
+
--- player.py
+++ player.py
@@ -1,2 +1,48 @@
+import pygame
+
 class Player:
 	def __init__ (self):
+		self.image = pygame.image.load("Art Stuff\player sprite.png").convert_alpha()
+		self.rect = self.image.get_rect()
+		self.rect.x = 288
+		self.rect.y = 208
+		self.x_velocity = 5
+		self.y_velocity = 5
+		self.moving = [False, False, False, False] #up, down, left, right
+	
+	def update(self):
+		if self.moving[0]:
+			future = self.rect.move(0, -self.y_velocity)
+			if future.top < 0:
+				self.rect.top = 0
+				
+			else:
+				self.rect = future
+				
+		elif self.moving[1]:
+			future = self.rect.move(0, self.y_velocity)
+			if future.bottom > 800:
+				self.rect.bottom = 800
+				
+			else:
+				self.rect = future
+				
+		if self.moving[2]:
+			future = self.rect.move(-self.x_velocity, 0)
+			if future.left < 0:
+				self.rect.left = 0
+				
+			else:
+				self.rect = future
+				
+		elif self.moving[3]:
+			future = self.rect.move(self.x_velocity, 0)
+			if future.right > 600:
+				self.rect.right = 600
+				
+			else:
+				self.rect = future
+				
+		
+	def draw(self, screen):
+		screen.blit(self.image, self.rect, [0, 0, 32, 48])
