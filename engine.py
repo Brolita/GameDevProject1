e64559ae55174342410eda02950fcc8b364dbfd5
brolita <byrnec3@rpi.edu>
2014-09-10 16:42:43 -0400

    Fixed

--- engine.py
+++ engine.py
@@ -14,16 +14,33 @@ class Engine:
 				for j in self.gameObjects:
 					if i != j and j.name != 'Tracers' and j.name != 'TracersOffset':	
 						if i.name == 'Player' and j.name == 'Bullet':
+						# check if Player has been hit by a bullet
 							collisionDetected = j.get_rect().collidepoint((i.getPosition().x, i.getPosition().y))
 							if collisionDetected == True:
+								self.gameObjects.remove(j)
 								print 'Player has been hit!'
+								
+						# check if Player has shot an enemy
+						if i.name == 'Snowball' and j.name == 'Enemy':
+							collisionDetected = j.get_rect().colliderect(i.get_rect())
+							if collisionDetected == True:
+								print 'Player shot an enemy!'
+								self.gameObjects.remove(i)
+								self.gameObjects.remove(j)
+																		   
+						# check if Player has collided with an enemy
+						if i.name == 'Player' and j.name == 'Enemy':
+							collisionDetected = j.get_rect().collidepoint((i.getPosition().x, i.getPosition().y))
+							if collisionDetected == True:
+								print 'Player has collided with an enemy!'
 						
 	def draw(self):
 		for i in range(len(self.gameObjects)):
 			self.gameObjects[i].draw(self.screen)
 		
-		while self.flags:
-			self.gameObjects.remove(self.flags.pop())
+		if self.flags:
+			while self.flags:
+				self.gameObjects.remove(self.flags.pop())
 		
 	def flag(self, obj):
 		self.flags.append(obj)
